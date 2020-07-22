7652834f78a435e17d96b047911cea86243e4a43
MohammadHossein <mhosein_mola_1380@yahoo.com>
2020-07-22 19:24:02 +0430

    oon package ha ghabli ro hazf kardam Model View Controller gozashtam hamoon MVC

---
+++ src/View/MainPage.java
@@ -0,0 +1,16 @@
+package View;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainPage extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

96160df8c2ed69e490f9ec42159667d4452dcbc9
MohammadHossein <mhosein_mola_1380@yahoo.com>
2020-07-23 07:19:14 +0430

    new folders

--- src/View/MainStage.java
+++ src/View/MainStage.java
@@ -3,11 +3,10 @@ package View;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class MainPage extends Application {
-
+public class MainStage extends Application {
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws Exception {
+        
     }
 
     public static void main(String[] args) {
